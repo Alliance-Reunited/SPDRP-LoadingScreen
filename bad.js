fbc47eab1e925ba08d3cd43cd9472eeadff5ba36
Orange Lion <phixmeplease@gmail.com>
2021-07-11 23:15:34 -0400

    text timers

--- bad.js
+++ bad.js
@@ -4,12 +4,29 @@
 */
 
 var id = "loadingText";
+var textID = "changeMe";
+
+var texts = [
+    'Because Family Guy wasnt good enough',
+    'I am on quack',
+    'They killed kenny!',
+    'Become your dream job, a trash man!',
+    'Cartman = fat, change my mind',
+]
 
 function SetStatusChanged(newStatus) {
     var ele = document.getElementById(id);
     ele.innerHTML = newStatus;
 }
 
+function randomiseText() {
+    var ele = document.getElementById(textID);
+    ele.innerHTML = texts[Math.floor(Math.random()*texts.length)];
+}
+
 window.onload = function() {
     SetStatusChanged("Waiting...");
+    randomiseText();
+
+    setInterval(randomiseText, 10000);
 }
