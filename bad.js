0d69c2b88b13b88c7d86541fa4d10c19f160d26d
Orange Lion <phixmeplease@gmail.com>
2021-07-12 01:17:05 -0400

    new loading gif, unused. Updates to CSS and additional stuff

--- bad.js
+++ bad.js
@@ -7,11 +7,21 @@ var id = "loadingText";
 var textID = "changeMe";
 
 var texts = [
-    'Because Family Guy wasnt good enough',
-    'I am on quack',
-    'They killed kenny!',
-    'Become your dream job, a trash man!',
-    'Cartman = fat, change my mind',
+    '“I am a cop, and you will respect my authoritah!” – Cartman',
+    '“Don’t you dare call me a Cartman!” – Stan',
+    '“Don’t lie, Stan. Lying makes you sterile.” – Mr. Garrison',
+    '“I’m not fat, I’m festively plump.” –Cartman',
+    '“Drugs are bad, mmkay?” –Mr. Mackey',
+    '“Screw you guys, I’m going home!” –Cartman',
+    '"Hell, everything’s legal in Mexico. It’s the American way!” –Jimbo',
+    '“Operation Human Shield my ass.” –Chef',
+    '“Cartman doesn’t know a rainforest from a Pop-Tart.” –Stan',
+    '*Muffled* “FUCK!” –Kenny',
+    '“Don’t you know the first law of physics? Anything that’s fun costs at least eight dollars.” –Cartman',
+    '“No, that’s wrong, Cartman. But don’t worry. There are no stupid answers, just stupid people.” –Mr. Garrison',
+    '“I haven’t taken the time to be secure with myself.” –Satan',
+    '"This server sucks ass" -Someone',
+    'Because Family Guy wasnt enough',
 ]
 
 function SetStatusChanged(newStatus) {
