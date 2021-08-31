a7e5856827201c7a36abcebe5c368fa3e7bdc3cf
Damien Vesper <ldamienvesper@gmail.com>
2021-08-31 08:58:47 -0500

    index.js: cleanup

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,63 +1,75 @@
-//
+/**
+ * Texts to randomly display while loading.
+ */
 const texts = [
-    "Cartman: “I am NOTHING like Family Guy! When I make jokes they are inherent to a story! Deep situational and emotional jokes based on what is relevant and has a point, not just one random interchangeable joke after another!”",
-    "“I am a cop, and you will respect my authoritah!” – Cartman",
-    "“Don’t you dare call me a Cartman!” – Stan",
-    "“Don’t lie, Stan. Lying makes you sterile.” – Mr. Garrison",
-    "“I’m not fat, I’m festively plump.” –Cartman",
-    "“Drugs are bad, mmkay?” –Mr. Mackey",
-    "“All animals kill, and the animals that don’t kill are stupid ones like cows and turtles and stuff.” –Kyle",
-    "“Screw you guys, I’m going home!” –Cartman",
-    "“Hell, everything’s legal in Mexico. It’s the American way!” –Jimbo"
+    `Cartman: “I am NOTHING like Family Guy! When I make jokes they are inherent to a story! Deep situational and emotional jokes based on what is relevant and has a point, not just one random interchangeable joke after another!”`,
+    `“I am a cop, and you will respect my authoritah!” – Cartman`,
+    `“Don’t you dare call me a Cartman!” – Stan`,
+    `“Don’t lie, Stan. Lying makes you sterile.” – Mr. Garrison`,
+    `“I’m not fat, I’m festively plump.” –Cartman`,
+    `“Drugs are bad, mmkay?” –Mr. Mackey`,
+    `“All animals kill, and the animals that don’t kill are stupid ones like cows and turtles and stuff.” –Kyle`,
+    `“Screw you guys, I’m going home!” –Cartman`,
+    `“Hell, everything’s legal in Mexico. It’s the American way!” –Jimbo`
 ];
 
-let count = 0;
-let index = 0;
-let currentText = 0;
-let letter = 0;
+/**
+ * Backgrounds to randomly display while loading.
+ */
+const backgrounds = [
+    `assets/img/background.jpg`,
+    `assets/img/background2.jpg`,
+    `assets/img/background3.jpg`,
+    `assets/img/background4.jpg`,
+    `assets/img/background5.jpg`,
+    `assets/img/background6.jpg`
+];
 
-function type () {
-    if (count === texts.length) {
-        count = 0;
-    }
-    currentText = texts[count];
-    letter = currentText.slice(0, ++index);
+let curText = ``;
+let curLetter = ``;
 
-    document.querySelector(".tips").textContent = letter;
+let textIndex = 0;
+let letterIndex = 0;
+let backgroundIndex = 0;
+
+/**
+ * Type out the current text.
+ */
+const type = () => {
+    if (count === texts.length) count = 0;
+
+    curText = texts[textIndex];
+    curLetter = curText.slice(0, ++index);
+
+    document.querySelector(`.tips`).textContent = curLetter;
+
     if (letter.length === currentText.length) {
-        count++;
-        index = 0;
-        setTimeout(type, 1000)
+        textIndex++;
+        letterIndex = 0;
+
+        setTimeout(type, 1e3);
         return;
     }
-    setTimeout(type, 50)
-}
 
-// Random Backgrounds
-const backgrounds = [
-    "assets/img/background.jpg",
-    "assets/img/background2.jpg",
-    "assets/img/background3.jpg",
-    "assets/img/background4.jpg",
-    "assets/img/background5.jpg",
-    "assets/img/background6.jpg",
-]
-let curBackground = 0;
+    setTimeout(type, 50);
+};
 
-function cycleBackground() {
-    if (curBackground === backgrounds.length) {
-        curBackground = 0;
-    }
+/**
+ * Cycle through the current background.
+ */
+const cycleBackground = () => {
+    if (backgroundIndex === backgrounds.length) backgroundIndex = 0;
 
-    document.body.style.background = `#f3f3f3 url('${backgrounds[curBackground]}') no-repeat center center fixed`;
-    document.body.style["background-size"] = "cover";
+    document.body.style.background = `#f3f3f3 url('${backgrounds[backgroundIndex]}') no-repeat center center fixed`;
+    document.body.style[`background-size`] = `cover`;
 
-    curBackground++;
-    setTimeout(cycleBackground, 7000)
-}
+    backgroundIndex++;
+};
 
-// Load Everything
+// Initialize.
 window.onload = () => {
     type();
     cycleBackground();
-}
+
+    setInterval(cycleBackground, 7e3);
+};
